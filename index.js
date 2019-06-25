533d3e4733068a0a94141bf5d58fb255dec7d94f
mcoons <mcoons67@gmail.com>
2019-06-24 20:17:28 -0600

    adding data page

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+  connectionString: process.env.DATABASE_URL
+  // ssl: true
 });
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/data', (req, res) => res.render('pages/data'))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -24,4 +28,5 @@ express()
       res.send("Error " + err);
     }
   })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
