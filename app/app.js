b2fadcac6c7aaf31ca0b17e753473b47472a2144
mcoons <mcoons67@gmail.com>
2019-06-25 23:11:08 -0600

    adding db

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
-const localport = 3001;
+const localport = 5000;
+
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true
+// });
 
 app.set('port', process.env.PORT || localport);
 app.set('view engine', 'ejs');
@@ -12,6 +18,7 @@ app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/projects'));
 app.use(require('./routes/contact'));
+app.use(require('./routes/db'));
 
 var server = app.listen(app.get('port'), function(){
     console.log("listening on port: " + app.get('port'));
---
+++ app/routes/db.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+router.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+    //   const results = { 'results': (result) ? result.rows : null};
+
+      res.render('db', { 'results': (result) ? result.rows : null,
+                         jumboPic: '/images/photos/denver.jpg',
+                         pageTitle: 'Data',
+                         pageID: 'data'
+                } );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+
+});
+
+module.exports = router;
