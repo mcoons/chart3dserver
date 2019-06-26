f25794fc116b15e26c194115fda19d347646c609
mcoons <mcoons67@gmail.com>
2019-06-25 21:56:57 -0600

    adding socket.io

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const server = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -8,8 +8,8 @@ const pool = new Pool({
   ssl: true
 });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+server()
+  .use(server.static(path.join(__dirname, 'public')))
 
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
@@ -29,5 +29,13 @@ express()
       res.send("Error " + err);
     }
   })
-
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  const io = socketIO(server);
+
+  io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+  });
+
+  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
