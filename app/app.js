1b0337dd09a3ae27fb68f2a99b7e10f7371c82d6
mcoons <mcoons67@gmail.com>
2019-06-26 19:02:03 -0600

    adding socket.io

--- app/app.js
+++ app/app.js
@@ -2,17 +2,17 @@ var express = require("express");
 var app = express();
 const localport = 5000;
 
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true
-// });
 
 app.set('port', process.env.PORT || localport);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.locals.siteTitle = "Michael Coons - Software Engineer Site";
+
+
+
+
+
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
@@ -23,3 +23,17 @@ app.use(require('./routes/db'));
 var server = app.listen(app.get('port'), function(){
     console.log("listening on port: " + app.get('port'));
 });
+
+
+var socketIO = require("socket.io");
+var ioServer = socketIO(server);
+
+ioServer.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+  });
+
+setInterval(() => ioServer.emit('time', new Date().toTimeString()), 1000);
+
+
+
--- app/routes/db.js
+++ app/routes/db.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const { Pool } = require('pg');
 
+const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
@@ -11,7 +11,6 @@ router.get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM test_table');
-    //   const results = { 'results': (result) ? result.rows : null};
 
       res.render('db', { 'results': (result) ? result.rows : null,
                          jumboPic: '/images/photos/denver.jpg',
@@ -19,7 +18,8 @@ router.get('/db', async (req, res) => {
                          pageID: 'data'
                 } );
       client.release();
-    } catch (err) {
+    } 
+    catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
