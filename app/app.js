e0eef7362f14cea4d2908eaa1b2fd65c2b8bb602
mcoons <mcoons67@gmail.com>
2019-06-26 19:38:39 -0600

    adding socket.io

--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
-var express = require("express");
+// var express = require("express");
+// var app = express();
+
+
+var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var ioServer = require('socket.io')(http);
+
+
+
+
+
+
+
+
+
 const localport = 5000;
+
 
 
 app.set('port', process.env.PORT || localport);
@@ -11,29 +27,32 @@ app.locals.siteTitle = "Michael Coons - Software Engineer Site";
 
 
 
-
-
-
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/projects'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/db'));
 
-var server = app.listen(app.get('port'), function(){
+
+
+var server = http.listen(app.get('port'), function(){
     console.log("listening on port: " + app.get('port'));
 });
 
 
-var socketIO = require("socket.io");
-var ioServer = socketIO(server);
+// var socketIO = require("socket.io");
+// var ioServer = socketIO(server);
 
 ioServer.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
   });
 
-setInterval(() => ioServer.emit('time', new Date().toTimeString()), 1000);
+setInterval(
+    () => { ioServer.emit('time', new Date().toTimeString());
+    console.log('sending time');
+}, 1000)
+
 
 
 
