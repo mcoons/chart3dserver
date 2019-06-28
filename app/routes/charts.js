8f6dc24f7e20da22e375d6f4c9168fc137331fd4
mcoons <mcoons67@gmail.com>
2019-06-28 00:00:24 -0600

    adding charts

--- app/routes/charts.js
+++ app/routes/charts.js
@@ -6,57 +6,25 @@ var smtpTransport = require('nodemailer-smtp-transport');
 
 
 
-
-
 router.get('/charts', async (req, res) => {
     try {
 
-      console.log('trying to send mail.');
+      // console.log('trying to send mail.');
 
-
-
-      // var nodemailer = require('nodemailer');
-      // var smtpTransport = require('nodemailer-smtp-transport');
       
-      var transporter = nodemailer.createTransport(smtpTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        auth: {
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD
-        }
-      }));
-      
-      var mailOptions = {
-        from: 'mc@michaelcoons.tech',
-        to: 'mcoons67@gmail.com',
-        subject: 'Sending Email using Node.js[nodemailer]',
-        text: 'That was easy!'
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });  
-
-
-
-
-      // var transporter = nodemailer.createTransport({
-      //   service: 'Gmail',
+      // var transporter = nodemailer.createTransport(smtpTransport({
+      //   service: 'gmail',
+      //   host: 'smtp.gmail.com',
       //   auth: {
       //     user: process.env.MAIL_USERNAME,
       //     pass: process.env.MAIL_PASSWORD
       //   }
-      // });
-
+      // }));
+      
       // var mailOptions = {
       //   from: 'mc@michaelcoons.tech',
       //   to: 'mcoons67@gmail.com',
-      //   subject: 'Sending Email using Node.js',
+      //   subject: 'Sending Email using Node.js[nodemailer]',
       //   text: 'That was easy!'
       // };
       
@@ -66,7 +34,7 @@ router.get('/charts', async (req, res) => {
       //   } else {
       //     console.log('Email sent: ' + info.response);
       //   }
-      // });
+      // });  
 
 
 
