dd1bef2247e6e749802b0baa6e4e7f98c292aead
mcoons <mcoons67@gmail.com>
2019-10-07 21:29:55 -0600

    adding data entry

--- app/routes/db.js
+++ app/routes/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 router.get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM chart_data');
+      const result = await client.query('SELECT * FROM chart_data order by id desc');
 
       res.render('db', { 'results': (result) ? result.rows : null,
                          jumboPic: '/images/photos/denver.jpg',
